b548b2b51f5991b38b2af5248c7bccfc1d211a27
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-02-22 16:22:49 +0700

    Change path urls of project and polls

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('poll/', views.index, name='index'),
-    path('poll/<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('poll/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('poll/<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/', views.index, name='index'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
